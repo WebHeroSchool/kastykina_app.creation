e3d65cbd7f213581c2565b750f23550c18b6a0e7
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-09-29 12:28:02 +0300

    header_menu

---
+++ script.js
@@ -0,0 +1,13 @@
+const header = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu__item');
+
+header.addEventListener('click', function(event) {
+  if (!event.target.classList.contains('menu__item'))         // если клик не по ссылке, просто завершим выполнение 
+    return; 
+  event.preventDefault();     // отменим стандартную реакцию ссылки на клик 
+  
+  menuItems.forEach(menuItem => {
+    menuItem.classList.remove('active'); // удалим класс active у всех наших ссылок...
+    event.target.classList.add('active'); // ...и добавим его той, по которой выполнен клик
+  })
+});
