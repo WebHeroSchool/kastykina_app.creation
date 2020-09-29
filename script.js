4c464cdcb0f8afe88bf618ab6a26099ad0afa162
Galina Kastykina <galina_kastykina@users.noreply.github.com>
2020-09-29 12:40:51 +0300

    codacy

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const header = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menu__item');
+const header = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu__item");
 
-header.addEventListener('click', function(event) {
-  if (!event.target.classList.contains('menu__item'))         // если клик не по ссылке, просто завершим выполнение 
+header.addEventListener("click", function(event) {
+  if (!event.target.classList.contains("menu__item"))         // если клик не по ссылке, просто завершим выполнение 
     return; 
   event.preventDefault();     // отменим стандартную реакцию ссылки на клик 
   
   menuItems.forEach(menuItem => {
-    menuItem.classList.remove('active'); // удалим класс active у всех наших ссылок...
-    event.target.classList.add('active'); // ...и добавим его той, по которой выполнен клик
+    menuItem.classList.remove("active"); // удалим класс active у всех наших ссылок...
+    event.target.classList.add("active"); // ...и добавим его той, по которой выполнен клик
   })
 });
